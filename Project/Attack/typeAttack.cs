99ac44e01a6a76ae2eb47cfb668af2b1100494bb
PoloBongo <earthrp.polo@gmail.com>
2024-01-29 22:18:16 +0100

    type attack haki commencé

---
+++ Project/Attack/typeAttack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Project.Attack
+{
+    
+    public enum AttackType
+    {
+        FruitParamecia,
+        FruitZoan,
+        FruitLogia,
+        Haki,
+        Armes
+    }
+    public class Attack
+    {
+        public string name { get; set; }
+        public AttackType type { get; set;}
+
+        public virtual void Execute()
+        {
+            Console.WriteLine($"Nom de l'attaque : {name} ||| type : {type}");
+        }
+    }
+
+    public class Haki : Attack
+    {
+        public int damage { get; set; }
+        public int countEndurance { get; set; }
+
+        public override void Execute()
+        {
+            base.Execute();
+            Console.WriteLine($"Dégats : {damage}");
+        }
+    }
+}
--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,108 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Bongo!");
+﻿using Project.Attack;
+using System;
+using static System.Net.Mime.MediaTypeNames;
+
+class Program
+{
+    static void Main()
+    {
+        Haki attack = new Haki { name = "Haki Observation", type = AttackType.Haki, damage = 10 };
+
+        Console.WindowWidth = 40; // Ajuster la largeur de la console
+        Console.WindowHeight = 20; // Ajuster la hauteur de la console
+
+        char[,] carte =
+        {
+            {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'},
+            {'#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#'},
+            {'#', '.', '.', '#', '#', '#', '#', '#', '#', '#', '#', '#', '.', '.', '.', '.', '.', '.', '#', '#'},
+            {'#', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '#', '#', '#', '.', '.', '.', '#', '#'},
+            {'#', '.', '.', '#', '.', '#', '#', '#', '#', '.', '.', '.', '.', '.', '#', '.', '.', '.', '#', '#'},
+            {'#', '.', '.', '#', '.', '.', '.', '.', '#', '#', '#', '#', '#', '.', '#', '.', '#', '.', '.', '#'},
+            {'#', '.', '.', '#', '.', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#', '.', '.', '#'},
+            {'#', '.', '.', '#', '#', '.', '#', '.', '#', '#', '#', '#', '#', '#', '#', '.', '#', '#', '.', '#'},
+            {'#', '.', '.', '.', '#', '.', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', '.', '.', '.', '#'},
+            {'#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'}
+        };
+
+
+        /* pos du départ du joueur */
+        int posX = 1;
+        int posY = 1;
+
+        /* Afficher la map au start */
+        AfficherCarte(carte, posX, posY);
+
+        static void AfficherCarte(char[,] carte, int posX, int posY)
+        {
+            for (int i = 0; i < carte.GetLength(0); i++)
+            {
+                for (int j = 0; j < carte.GetLength(1); j++)
+                {
+                    if (i == posY && j == posX)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write("P ");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        Console.Write(carte[i, j] + " ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        ConsoleKeyInfo keyInfo;
+
+        do
+        {
+            keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.Z:
+                    if (posY > 1 && carte[posY - 1, posX] == '.')
+                    {
+                        carte[posY, posX] = '.';
+                        posY--;
+                    }
+                    break;
+                case ConsoleKey.S:
+                    if (posY < carte.GetLength(0) - 2 && carte[posY + 1, posX] == '.')
+                    {
+                        carte[posY, posX] = '.';
+                        posY++;
+                    }
+                    break;
+                case ConsoleKey.Q:
+                    if (posX > 1 && carte[posY, posX - 1] == '.')
+                    {
+                        carte[posY, posX] = '.';
+                        posX--;
+                    }
+                    break;
+                case ConsoleKey.D:
+                    if (posX < carte.GetLength(1) - 2 && carte[posY, posX + 1] == '.')
+                    {
+                        carte[posY, posX] = '.';
+                        posX++;
+                    }
+                    break;
+            }
+
+            Console.Clear();
+            AfficherCarte(carte, posX, posY);
+            
+            attack.Execute();
+            attack.damage -= 1;
+
+        } while (keyInfo.Key != ConsoleKey.Escape);
+
+        Console.ReadLine();
+
+
+
+    }
+}
